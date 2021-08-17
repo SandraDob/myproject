1a8d6d0b2cb4add1aa0b014541d8b1198c20c478
SandraDob <sandra.dobric@hotmail.ch>
2021-08-17 22:44:31 +0200

    removed console logs

--- src/javscript.js
+++ src/javscript.js
@@ -72,7 +72,6 @@ let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
